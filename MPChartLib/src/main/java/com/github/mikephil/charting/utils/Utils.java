6658d0ecf8f3e628caa429a3cec2750c64ff274e
mohamad-shafaee <38423442+mohamad-shafaee@users.noreply.github.com>
2019-07-15 12:27:45 +0430

    Update Utils.java
    The mDefaultValueFormatter has not been declared but it has been returned by getDefaultValueFormatter() function! I declared and also initiated it.

--- MPChartLib/src/main/java/com/github/mikephil/charting/utils/Utils.java
+++ MPChartLib/src/main/java/com/github/mikephil/charting/utils/Utils.java
@@ -38,6 +38,7 @@ public abstract class Utils {
     private static int mMaximumFlingVelocity = 8000;
     public final static double DEG2RAD = (Math.PI / 180.0);
     public final static float FDEG2RAD = ((float) Math.PI / 180.f);
+    private final static ValueFormatter mDefaultValueFormatter = new ValueFormatter();
 
     @SuppressWarnings("unused")
     public final static double DOUBLE_EPSILON = Double.longBitsToDouble(1);
